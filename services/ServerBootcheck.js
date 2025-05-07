1190248c8eac6cb6e199e9ad2176553d2623dc34
lincoln0623 <lincolnlim267@gmail.com>
2025-05-07 17:57:05 +0800

    Removed directory headers.

--- services/ServerBootcheck.js
+++ services/ServerBootcheck.js
@@ -16,7 +16,7 @@ const fs = require("fs");
 //     process.exit(1);
 // }
 
-const saKeyPath = path.resolve(__dirname, "../etc/secrets/serviceAccountKey.json");
+const saKeyPath = path.resolve("../etc/secrets/serviceAccountKey.json");
 
 if (!fs.existsSync(saKeyPath)) {
     console.log(`\n[BOOTCHECK] - FAILED: Service Account Key missing: ${saKeyPath}\n`);
